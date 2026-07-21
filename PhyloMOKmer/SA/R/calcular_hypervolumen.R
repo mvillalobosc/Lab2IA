23018db732325b99704b16ab50dcaf771596dc51
mvillalobosc <97206322+mvillalobosc@users.noreply.github.com>
2026-07-20 20:49:55 -0400

    Delete PhyloMOKmer directory

--- PhyloMOKmer/SA/R/calcular_hypervolumen.R
+++
@@ -1,8 +0,0 @@
-# =====================================================================
-# calcular_hypervolumen(arbol)  ·  HV de una solucion, ref = (2,2)
-# =====================================================================
-calcular_hypervolumen <- function(arbol) {
-  puntajes <- cbind(arbol$scores$me_norm, arbol$scores$ls_norm)
-  arbol$scores$hyp <- round(computeHV(t(puntajes), ref.point = c(2, 2)), 4)
-  arbol
-}
--- PhyloMOKmer/SA/R/calcular_objetivos.R
+++
@@ -1,8 +0,0 @@
-# =====================================================================
-# calcular_objetivos(arbol)  ·  ME y LS de un unico arbol
-# =====================================================================
-calcular_objetivos <- function(arbol) {
-  arbol$scores$me <- round(sum(arbol$edge.length), 4)
-  arbol$scores$ls <- round(sum(abs(arbol$dist_kmer - cophenetic.phylo(arbol))), 4)
-  arbol
-}
